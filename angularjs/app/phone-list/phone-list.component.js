d1434351fb641f762c6b2587366c3a2d7dba3bca
zhanglianxin <zhanglianxin1993@gmail.com>
2017-05-11 14:36:46 +0800

    Fetch data from server

--- angularjs/app/phone-list/phone-list.component.js
+++ angularjs/app/phone-list/phone-list.component.js
@@ -2,22 +2,11 @@
 angular.module('phoneList').component('phoneList', {
     // Using an external template
     templateUrl: 'phone-list/phone-list.template.html',
-    controller: function PhoneListController() {
-        this.phones = [
-            {
-                name: 'Nexus S',
-                snippet: 'Fast just got faster with Nexus S.',
-                age: 1
-            }, {
-                name: 'Motorola XOOM™ with Wi-Fi',
-                snippet: 'The Next, Next Generation tablet.',
-                age: 2
-            }, {
-                name: 'MOTOROLA XOOM™',
-                snippet: 'The Next, Next Generation tablet.',
-                age: 3
-            }
-        ];
+    controller: function PhoneListController($http) {
+        var self = this;
+        $http.get('phones/phones.json').then(function(response) {
+            self.phones = response.data;
+        });
 
         this.orderProp = 'age';
     }
