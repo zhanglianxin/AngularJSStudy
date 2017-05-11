b90ce2b94a4e96a9ed7d282c2aa39adcfd483512
zhanglianxin <zhanglianxin1993@gmail.com>
2017-05-11 14:38:33 +0800

    Solve the DI problem on minification

--- angularjs/app/phone-list/phone-list.component.js
+++ angularjs/app/phone-list/phone-list.component.js
@@ -2,12 +2,15 @@
 angular.module('phoneList').component('phoneList', {
     // Using an external template
     templateUrl: 'phone-list/phone-list.template.html',
-    controller: function PhoneListController($http) {
-        var self = this;
-        $http.get('phones/phones.json').then(function(response) {
-            self.phones = response.data;
-        });
+    controller: [
+        '$http',
+        function PhoneListController($http) {
+            var self = this;
+            $http.get('phones/phones.json').then(function(response) {
+                self.phones = response.data;
+            });
 
-        this.orderProp = 'age';
-    }
+            this.orderProp = 'age';
+        }
+    ]
 });
