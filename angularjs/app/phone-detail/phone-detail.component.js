6d249111ecdae31efc9c223d56cc04559da3e339
zhanglianxin <zhanglianxin1993@gmail.com>
2017-05-12 15:01:54 +0800

    Add event handler to the thumbnail

--- angularjs/app/phone-detail/phone-detail.component.js
+++ angularjs/app/phone-detail/phone-detail.component.js
@@ -4,10 +4,14 @@ angular.module('phoneDetail').component('phoneDetail', {
         '$http', '$routeParams',
         function PhoneDetailController($http, $routeParams) {
             var self = this;
+            self.setImage = function setImage(imageUrl) {
+                self.mainImageUrl = imageUrl;
+            };
             $http.get('//angular.github.io/angular-phonecat/step-10/app/' +
                     'phones/' + $routeParams.phoneId + '.json')
                 .then(function(response) {
                     self.phone = response.data;
+                    self.setImage(self.phone.images[0]);
                 });
         }
     ]
